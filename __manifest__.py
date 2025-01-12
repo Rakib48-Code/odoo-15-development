8601401a999a50e4853f5d09e3faa33ddd8b8413
Rakib-Odoo <shahidulhuda0@gmail.com>
2025-01-12 22:02:38 +0600

    create manifest file

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'Odoo 15 Develpoment',
+    'version' : '15.0.0.1.1',
+    'category' : 'Odoo 15 Tutorial',
+    'summary':'Development Tutorial',
+    'description': 'Odoo 15 full development tutorials.',
+    'depends' : [],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
