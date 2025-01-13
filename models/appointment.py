c7a2df5547fb43bd5bd044a5005f0d0536288aa2
Rakib-Odoo <shahidulhuda0@gmail.com>
2025-01-14 00:15:43 +0600

    state color add done

--- models/appointment.py
+++ models/appointment.py
@@ -1,10 +1,46 @@
 from odoo import api, fields, models
 
+
 class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
-    appointment_time = fields.Datetime(string='Appointment Time')
-    booking_date = fields.Date(string='Booking Date')
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], string='Gender', related='patient_id.gender')
+    ref = fields.Char(string='Reference')
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], string="Status")
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
+
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_in_consultation(self):
+        self.state = 'in_consultation'
+
+    def action_done(self):
+        self.state = 'done'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+
--- models/patient.py
+++ models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
@@ -6,10 +7,38 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread','mail.activity.mixin']
 
     name = fields.Char(string='Name', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    date_birth = fields.Date(string='Date Birth')
+    age = fields.Integer(string='Age', compute='compute_age')
     gender = fields.Selection([
         ('male','Male'),
         ('female','Female'),
-    ], string='Gender')
-    ref = fields.Char(string='Reference', tracking=True)
+    ], string='Gender', default='male')
+    ref = fields.Char(string='Reference', tracking=True, default='New Patient')
     active = fields.Boolean(string='Active', default=True)
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], string="Status")
+
+    @api.onchange('date_birth')
+    def compute_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_birth:
+                rec.age = today.year - rec.date_birth.year
+            else:
+                rec.age = 0
+
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_in_consultation(self):
+        self.state = 'in_consultation'
+
+    def action_done(self):
+        self.state = 'done'
+
+    def action_cancel(self):
+        self.state = 'cancel'
